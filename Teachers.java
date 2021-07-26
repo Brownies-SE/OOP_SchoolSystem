75ce40ba83656eaa3bec10d4091e7637ff4e630a
Josh Brown <brownjoshalan@gmail.com>
2021-07-26 10:55:14 -0600

    created the inital object for the teachers

---
+++ Teachers.java
@@ -0,0 +1,24 @@
+public class Teachers {
+
+    private int id;
+    private String name;
+    private int salary;
+
+    public Teachers( int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public getId() {
+        return id;
+    }
+
+    public getName() {
+        return name;
+    }
+
+    public getSalary() {
+        return salary;
+    }
+}
