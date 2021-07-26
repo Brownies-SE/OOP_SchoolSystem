954277b27e9a4dd7bff3a18cd4c30978934350c1
Josh Brown <brownjoshalan@gmail.com>
2021-07-26 11:18:14 -0600

    Added the school object, using teachers and students

--- School.java
+++ School.java
@@ -3,6 +3,27 @@ import java.util.List;
 public class School {
     
     private List<Teachers> teachers;
-    private List<Students> studentss;
+    private List<Students> students;
     
+
+    public School( List<Teachers> teachers, List<Students> students) {
+        this.teachers = teachers;
+        this.students = students;
+    }
+
+    public List<Teachers> getTeachers() {
+        return teachers;
+    }
+
+    public List<Students> getStudents() {
+        return students;
+    }
+
+    public void addTeacher(Teachers teacher) {
+        teachers.add(teacher);
+    }
+
+    public void addStudent(Students student) {
+        students.add(student);
+    }
 }
